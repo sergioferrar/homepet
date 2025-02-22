f6ee32f79f14efeb515c6b1f1abbbfe3027ce264
Adilio Gobira <adiliogobira@gmail.com>
2025-02-22 05:56:47 -0300

    dashboard

--- src/Repository/FinanceiroRepository.php
+++ src/Repository/FinanceiroRepository.php
@@ -2,8 +2,8 @@
 namespace App\Repository;
 
 use App\Entity\Financeiro;
-use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\Exception;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @extends ServiceEntityRepository<Financeiro>
