9655136004fe67b21f5ba69043260ddda3363015
Adilio Gobira <adiliogobira@gmail.com>
2025-04-15 21:14:46 -0300

    criando tela de planos

---
+++ src/Entity/Plano.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PlanoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PlanoRepository::class)
+ */
+class Plano
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titulo;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $descricao;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $valor;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $trial;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dataTrial;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dataPlano;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitulo(): ?string
+    {
+        return $this->titulo;
+    }
+
+    public function setTitulo(string $titulo): self
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    public function getDescricao(): ?string
+    {
+        return $this->descricao;
+    }
+
+    public function setDescricao(string $descricao): self
+    {
+        $this->descricao = $descricao;
+
+        return $this;
+    }
+
+    public function getValor(): ?string
+    {
+        return $this->valor;
+    }
+
+    public function setValor(string $valor): self
+    {
+        $this->valor = $valor;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getTrial(): ?int
+    {
+        return $this->trial;
+    }
+
+    public function setTrial(int $trial): self
+    {
+        $this->trial = $trial;
+
+        return $this;
+    }
+
+    public function getDataTrial(): ?\DateTimeInterface
+    {
+        return $this->dataTrial;
+    }
+
+    public function setDataTrial(\DateTimeInterface $dataTrial): self
+    {
+        $this->dataTrial = $dataTrial;
+
+        return $this;
+    }
+
+    public function getDataPlano(): ?\DateTimeInterface
+    {
+        return $this->dataPlano;
+    }
+
+    public function setDataPlano(\DateTimeInterface $dataPlano): self
+    {
+        $this->dataPlano = $dataPlano;
+
+        return $this;
+    }
+}
