ff0b795570f3b77fb1380abbb0a8841e3b52d34b
Sergio Ferrari <sergio.ferrari@sigmavaf.com.br>
2025-06-26 15:54:34 -0300

    ajuste

--- src/Service/DynamicConnectionManager.php
+++ src/Service/DynamicConnectionManager.php
@@ -25,7 +25,7 @@ class DynamicConnectionManager
         $params = $this->originalParams;
 
 
-        //$params['dbname'] = $newDbName;
+        $params['dbname'] = $newDbName;
         //if($params['host'] !== '127.0.0.1'){
             $params['user'] = $newUsername;
         //}
