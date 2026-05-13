e70f39a3f58a6dc25cd0bc5d76d2adb061714847
Adilio Gobira <adilio.gobira@sigmavaf.com.br>
2026-05-13 17:05:33 -0300

    corrigindo erro no cadastro / pagamento da tela de cadastro de estabelecimentos da landingpage

--- src/Controller/LandingpageController.php
+++ src/Controller/LandingpageController.php
@@ -10,6 +10,7 @@ use App\Service\Payment\MercadoPagoService;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class LandingpageController extends DefaultController
 {
