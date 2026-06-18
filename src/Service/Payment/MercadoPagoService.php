be27c88bf16a3739c819bdf881708edb0c75055a
Adilio Gobira <adilio.gobira@sigmavaf.com.br>
2026-06-18 07:59:58 -0300

    vendo logs

--- src/Service/Payment/MercadoPagoService.php
+++ src/Service/Payment/MercadoPagoService.php
@@ -81,7 +81,7 @@ class MercadoPagoService implements PaymentGatewayInterface
         // Data de início: imediatamente (formato ISO 8601 com timezone)
         $startDate = isset($data['start_date'])
             ? $data['start_date']
-            : (new \DateTime('now', new \DateTimeZone('America/Sao_Paulo')))->format('Y-m-d\TH:i:s.000P');
+            : (new \DateTime('+1 day', new \DateTimeZone('America/Sao_Paulo')))->format('Y-m-d\TH:i:s.000P');
 
         $body = [
             'reason'   => $data['title'] ?? 'Assinatura Sistema',
